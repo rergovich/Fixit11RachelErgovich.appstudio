2f48543daaf522398ffdc71d67404cbd82a9079e
rergovich <rachelergovich@creighton.edu>
2020-04-02 12:29:29 -0500

    Completed Fixit 11
    Only Programs 1-3 bc was on the Zoom session on Wednesday 4/1

--- forms/customerDelete/customerDelete.js
+++ forms/customerDelete/customerDelete.js
@@ -1,61 +1,68 @@
 
-let customerData = ''
-
 customerDelete.onshow=function(){
-  txtUpdate_contents.style.height = "100px"
-  query = `SELECT name FROM customer`
-  alert(query)
+  query = `SELECT * FROM customer`
+  //alert(query)
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=rje33080&pass=Bluejays$1!&database=rje33080&query=" + query)
   
   if (req.status == 200) {
-    customerData = JSON.parse(req.responseText)
+    results = JSON.parse(req.responseText)
     console.log(results)
-  
     if (results.length == 0)
       NSB.MsgBox(`There are no customers.`)
     else {
       console.log(`The parsed JSON is ${results}.`)
       console.log(`The first row/item in the big array is a small array: ${results[0]}.`)
       
-      let message = ""
         for (i = 0; i <= results.length - 1; i++) {
-          message = message + results[i][1] + "\n"
+          drpDelete.addItem(results[i][1]) 
         }
-      txtDelete.value = message
     }
   } else
       NSB.MsgBox(`Error code: ${req.status}.`)
 }
 
-btnDelete.onclick=function(){
-  let customerName = txtDelete.value
+drpDelete.onclick=function(s){
+   if (typeof (s) == 'object') {
+    return
+  } else {
+      drpDelete.value = s
+      customerName = drpDelete.selection
+      console.log(customerName)
   
-  let found = false
-  for (i = 0; i <= customerData.length - 1; i++) {
-    if (customerName == customerData[i][1]) {
-      found = true
-      break
-    }
-  }
-  if (found == false)
-    NSB.MsgBox(`That customer is not in the database.`)
-  
-    else if (found == true) {
-      let query = `DELETE FROM customer WHERE name = ${CustomerName}`
+      query = `DELETE FROM customer WHERE name = "${customerName}"`
       alert(query)
       req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=rje33080&pass=Bluejays$1!&database=rje33080&query=" + query)
-      
       if (req.status == 200) {
-        if (req.response.Text == 500) {
-          NSB.MsgBox(`You successfully deleted the customer named ${customerName}`)
-          let message = ''
-            for (i = 0; i <= results.length - 1; i++) {
-              message = message + results[i][1] + "\n"
-            }
+        results = JSON.parse(req.responseText)
+        if (req.responseText == 500) {
+          NSB.MsgBox(`The customer was deleted.`)
+          }
+          else {
+            NSB.MsgBox(`Could not delete customer.`)
+          }
+        } else
+            NSB.MsgBox(`Error code: ${req.status}.`)
+      }
+  query = `SELECT * FROM customer`
+  //alert(query)
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=rje33080&pass=Bluejays$1!&database=rje33080&query=" + query)
+  if (req.status == 200) {
+    results = JSON.parse(req.responseText)
+    console.log(results)
+    if (results.length == 0)
+      NSB.MsgBox(`There are no customers.`)
+    else {
+      //console.log(`The parsed JSON is ${results}.`)
+      //console.log(`The first row/item in the big array is a small array: ${results[0]}.`)
+        let message = ''
+        for (i = 0; i <= results.length - 1; i++) {
+          message = message + results[i][1] + "\n"
           txtDelete.value = message
-        } else 
-            NSB.MsgBox(`There was a problem deleting ${customerName} from the data` )
-    } else 
-        NSB.MsgBox(`Error: ${req.status}`)
+        }
+    }
   }
+}
+
+btnDelete.onclick=function(){
+  ChangeForm(customerAdd)
 }
--- forms/customerSelect/customerSelect.js
+++ forms/customerSelect/customerSelect.js
@@ -1,16 +1,17 @@
 let query = ''
 let req = ''
 let results = ''
+let customerData = []
 
 customerSelect.onshow=function(){
-  txtSelect_contents.style.height = "100px"
-  query = `SELECT name FROM customer`
-  alert(query)
+  query = `SELECT * FROM customer`
+  //alert(query)
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=rje33080&pass=Bluejays$1!&database=rje33080&query=" + query)
   
-  if (req.status ==200) {
+  if (req.status == 200) {
     results = JSON.parse(req.responseText)
     console.log(results)
+    customerData = results
   
     if (results.length == 0)
       NSB.MsgBox(`There are no customers.`)
@@ -18,50 +19,61 @@ customerSelect.onshow=function(){
       console.log(`The parsed JSON is ${results}.`)
       console.log(`The first row/item in the big array is a small array: ${results[0]}.`)
       
-      let name = ""
-        for (i = 0; i <= results.length - 1; i++) {
-          name = name + results[i][1] + "\n"
-        }
-      txtSelect.value = name
-    }
-  } else
-      NSB.MsgBox(`Error code: ${req.status}.`)
-}
-
-let queryState = ''
-
-btnSelect.onclick=function(){
-  queryState = `SELECT state FROM customer WHERE name = "${txtSelect.value}"`
-  alert(queryState)
-  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=rje33080&pass=Bluejays$1!&database=rje33080&query=" + queryState)
-  
-  if (req1.status ==200) {
-    state = JSON.parse(req1.responseText)
-    console.log(state)
-  } else
-      NSB.MsgBox(`Error code: ${req1.status}`)
-  
-  query = `SELECT * FROM customer WHERE state = ${state}`
-  alert(query)
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=rje33080&pass=Bluejays$1!&database=rje33080&query=" + query)
-  
-  if (req.status ==200) {
-    results = JSON.parse(req.responseText)
-    console.log(results)
-  
-    if (results.length == 0)
-      NSB.MsgBox(`There are no customers that match.`)
-    else {
-      console.log(`The parsed JSON is ${results}.`)
-      console.log(`The first row/item in the big array is a small array: ${results[0]}.`)
-      
       let message = ""
         for (i = 0; i <= results.length - 1; i++) {
-          message = message + results[i][1] + "\n"
-        }
-      txtSelect.value = message
+      drpSelect.addItem(results[i][1]) }
     }
   } else
       NSB.MsgBox(`Error code: ${req.status}.`)
 }
 
+drpSelect.onclick=function(s){
+  let customerName = ''
+  let state = ''
+  let foundNames = []
+  
+  if (typeof (s) == 'object') {
+    return
+  } else {
+      drpSelect.value = s
+      customerName = drpSelect.selection
+      //console.log(`customerName is ${customerName}`)
+      for (i = 0; i <= customerData.length - 1; i++) {
+        if (customerName == customerData[i][1]) {
+          state = customerData[i][4]
+          break
+        } //if
+      }//for
+      for (i = 0; i <= customerData.length - 1; i++) {
+        //console.log(`state is ${state} and customer data is ${customerData[i][4]}`)
+        if (state == customerData[i][4])
+          foundNames.push(customerData[i][1])
+      } //for
+        //console.log(`The state is ${state} and the names in same state are ${foundNames}`)
+        
+        //show customers with state same as chosen above
+          query = `SELECT name FROM customer WHERE state = "${state}"`
+          //alert(query)
+          req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=rje33080&pass=Bluejays$1!&database=rje33080&query=" + query)
+          if (req.status == 200) {
+            results = JSON.parse(req.responseText)
+            if (results.length == 0)
+              NSB.MsgBox(`There are no customers.`)
+            else {
+              //console.log(`The parsed JSON is ${results}.`)
+              //console.log(`The first row/item in the big array is a small array: ${results[0]}.`)
+              
+              let message = ""
+                for (i = 0; i <= results.length - 1; i++) {
+                  message = message + results[i][0] + "\n"
+              txtaSelect.value = message }
+            } //else
+          } else
+              NSB.MsgBox(`Error code: ${req.status}.`)
+    } // else not object
+  
+}
+
+btnSelect.onclick=function(){
+  ChangeForm(customerDelete)
+}
